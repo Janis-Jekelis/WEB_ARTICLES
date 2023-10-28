c53492e293e16ad141bbaa00c7af73fecb825c0a
Janis <janisjekelis@gmail.com>
2023-10-28 15:05:52 +0300

    WA

--- app/Controllers/ArticleController.php
+++ app/Controllers/ArticleController.php
@@ -1,1 +1,28 @@
 <?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use APP\Models\Article;
+use APP\Response;
+
+class ArticleController
+{
+    public function index(): Response
+    {
+        return new Response("Articles.index", [
+            "articles" => [
+                new Article("Title1", "Description1"),
+                new Article("Title2", "Description2"),
+                new Article("Title3", "Description3"),
+            ]
+        ]);
+
+    }
+    public function show():Response
+    {
+        return new Response("Articles.show",[
+            "article"=>new Article("TitleABC", "DescriptionABC"),
+        ]);
+    }
+}
